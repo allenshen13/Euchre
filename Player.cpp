1c390b71d3ebdde4d73726bc56f2a5c0405d2a47
rbaronia <rbaronia@umich.edu>
2022-02-18 15:39:06 -0500

    player

--- Player.cpp
+++ Player.cpp
@@ -3,9 +3,19 @@
 #include <iostream>
 #include <string>
 #include "Player.h"
+
 Player * Player_factory(const std::string &name, const std::string &strategy) {
   assert(false);
 }
 std::ostream & operator<<(std::ostream &os, const Player &p) {
   assert(false);
+}
+
+const std::string & Player::get_name() const {
+    assert(false);
+    
+}
+
+void Player::add_card(const Card &c) {
+    assert(false);
 }
