594b89f0b533c52b652957e46b4a9f2a4a57a8d6
rbaronia <rbaronia@umich.edu>
2022-02-17 20:01:46 -0500

    card

--- Pack.cpp
+++ Pack.cpp
@@ -5,8 +5,20 @@
 #include <string>
 #include "Pack.h"
 
+// EFFECTS: Initializes the Pack to be in the following standard order:
+//          the cards of the lowest suit arranged from lowest rank to
+//          highest rank, followed by the cards of the next lowest suit
+//          in order from lowest to highest rank, and so on.
+// NOTE: The standard order is the same as that in pack.in.
+// NOTE: Do NOT use pack.in in your implementation of this function
 Pack::Pack() {
-    assert(false);
+    Card c1(Card::RANK_NINE, Card::SUIT_SPADES);
+    Card c2(Card::RANK_TEN, Card::SUIT_SPADES);
+    Card c3(Card::RANK_JACK, Card::SUIT_SPADES);
+    Card c4(Card::RANK_QUEEN, Card::SUIT_SPADES);
+    Card c5(Card::RANK_KING, Card::SUIT_SPADES);
+    Card c6(Card::RANK_ACE, Card::SUIT_SPADES);
+    
 }
 
 Pack::Pack(std::istream& pack_input) {
